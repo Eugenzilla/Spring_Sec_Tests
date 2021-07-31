90a5bc29fa5291eac6db8b5ddc4528fb806d4897
EugenzillaTests <r3gzzz@gmail.com>
2021-07-31 20:55:05 +0200

    spring sec simple tests

--- src/main/java/com/example/securitytestapp/config/SpringSecurityConfig.java
+++ src/main/java/com/example/securitytestapp/config/SpringSecurityConfig.java
@@ -1,4 +1,19 @@
 package com.example.securitytestapp.config;
 
-public class SpringSecurityConfig {
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password("{noop}user7")
+                .roles("USER");
+
+    }
 }
